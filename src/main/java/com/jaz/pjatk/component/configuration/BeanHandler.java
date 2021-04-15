0178c05644c6b9c987cc03d6c4e835d682480403
Karol Kuchnio <kuchniokarol@gmail.com>
2021-04-16 00:14:20 +0200

    add logger

--- src/main/java/com/jaz/pjatk/component/configuration/BeanHandler.java
+++ src/main/java/com/jaz/pjatk/component/configuration/BeanHandler.java
@@ -4,6 +4,8 @@ package com.jaz.pjatk.component.configuration;
 import com.jaz.pjatk.model.ServiceObject;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +16,11 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(name="data.app.feature-status",havingValue = "true")
 public class BeanHandler {
 
+    private final static Logger log = LoggerFactory.getLogger(BeanHandler.class);
 
     @Bean
     public ServiceObject createBean(){
-        System.out.println("feature is enabled!");
+        log.info("feature is enabled!");
         return new ServiceObject();
     }
 }
