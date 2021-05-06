c477529ead7777fd8c91a8aa43161bf43d2cbc6a
Karol Kuchnio <kuchniokarol@gmail.com>
2021-05-06 23:32:56 +0200

    [Homework]

---
+++ src/main/java/com/jaz/pjatk/component/controller/HomeWorkRestController.java
@@ -0,0 +1,38 @@
+package com.jaz.pjatk.component.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/homework")
+public class HomeWorkRestController {
+
+    @GetMapping("/{pathVar}")
+    public ResponseEntity<String> getWithPathVariable(@PathVariable String pathVar){
+        return ResponseEntity.ok(pathVar);
+    }
+
+    @GetMapping("/query")
+    public ResponseEntity<String> getWithRequestParam(@RequestParam String value){
+        return ResponseEntity.ok(value);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<JsonNode> putWithPathVariable(@PathVariable Long id,@RequestBody JsonNode jsonNode){
+        return ResponseEntity.ok(((ObjectNode) jsonNode).put("id", id));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<JsonNode> postWithPathVariable(@RequestBody JsonNode jsonNode){
+        return ResponseEntity.ok(jsonNode);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Long id){
+        return ResponseEntity.ok().build();
+    }
+
+}
